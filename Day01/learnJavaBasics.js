ace23e63608b2d5239256863ce7f0dac78152672
PrabudasDevadoss <138124446+PrabudasDevadoss@users.noreply.github.com>
2026-03-01 08:51:50 +0530

    Add files via upload
    Day 1 practice

---
+++ Day01/learnJavaBasics.js
@@ -0,0 +1,17 @@
+console.log ("Hello world")
+var firstName = 'Prabudas'
+var companyName = "Testleaf"
+mobileNum = 1234567891
+isAutomation = true
+var hasPlaywright
+
+var phoneNo = null
+//console.log(typeof empAddress)
+console.log(typeof hasPlaywright)
+console.log(typeof phoneNo)
+
+console.log("Firstname : " + firstName + " Datatype : " +typeof firstName)
+console.log(typeof companyName)
+console.log(typeof mobileNum)
+console.log(typeof isAutomation)
+console.log(typeof hasPlaywright)
